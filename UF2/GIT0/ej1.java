f468c7b7610f02d6df8ef040dfc4e4a69f906363
acallejas22 <acallejas22@ilg.cat>
2022-10-10 17:38:51 +0200

    cambios

--- UF2/GIT0/ej1.java
+++ UF2/GIT0/ej1.java
@@ -5,7 +5,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 Joan Sèculi <jseculi@escoladeltreball.org>
+ * Copyright 2020 Joan Sèculi <acallejas22@ilg.cat>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
